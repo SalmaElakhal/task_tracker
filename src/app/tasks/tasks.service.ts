c3e94e61106483ca9ac57e58ea6ed8f903490e8a
Salma El-Akhal <salma.elakhal.pro@gmail.com>
2024-07-01 23:43:07 +0100

    with service

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { NewTaskData } from './task/task.model';
+import { type NewTaskData } from './task/task.model';
 
 @Injectable({ providedIn: 'root' })
 export class TasksService {
@@ -26,6 +26,14 @@ export class TasksService {
       dueDate: '2024-06-15',
     },
   ];
+  
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+    
+    if(tasks){
+      this.tasks = JSON.parse(tasks);
+    }
+  }
 
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
@@ -39,9 +47,16 @@ export class TasksService {
       summary: taskData.summary,
       dueDate: taskData.date,
     });
+    this.saveTasks();
+
   }
 
   removeTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
